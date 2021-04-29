7bc3eecadcd2a3907aeed12a394b8442fe60d7b6
Matvey <m.d.spiridonov@yandex.ru>
2021-04-29 13:37:37 +0300

    создание unit testa true

--- test_2.py
+++ test_2.py
@@ -3,7 +3,7 @@ import re
 import myform
 class Test_test_2(unittest.TestCase):
     def test_T(self):
-        list_mail = ["m.m@mail.ru", "m1@mail.ru","fgfg@mail.ru",'rrfr@mail.ru']
+        list_mail = ["m.m@mail.ru", "m1@mail.ru","fgfg@mail.ru",'rfr@mail.ru']
         for i in range (len(list_mail)):
             regex=re.search(r'[a-zA-Z0-9_.+-]+[@][a-zA-Z0-9-.]+[^.]\.[a-zA-Z0-9-.]{2,3}$', list_mail[i])
             if (regex==None):
