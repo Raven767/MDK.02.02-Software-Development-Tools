073cb07ea0ecd697a082ed5b3c4881af0d5a7fc8
Matvey <m.d.spiridonov@yandex.ru>
2021-04-29 14:06:41 +0300

    добавдение тестов

--- test_1.py
+++ test_1.py
@@ -3,9 +3,9 @@ import re
 import myform
 class Test_test_1(unittest.TestCase):
     def test_F(self):
-        list_mail = ["", "1", "m1@", "@mail", "regr@_ru", "@mail.r", "mail.ru", "@ru.mail","ma@il.ru"] 
+        list_mail = ["", "1", "m1@", "@mail", "regr@_ru", "@mail.r", "mail.ru", "@ru.mail",".@mail.ru","Я@ТЫ@mail.ru","@mail..ru"] 
         for i in range (len(list_mail)):
-            regex=re.search(r'[a-zA-Z0-9_.+-]+[@][a-zA-Z0-9-.]+[^.]\.[a-zA-Z0-9-.]{2,3}$', list_mail[i])
+            regex=re.search(r'[a-zA-Z0-9_+-]+[@][a-zA-Z0-9-.]+[^.]\.[a-zA-Z0-9-.]{2,3}$', list_mail[i])
             if (regex==None):
                 a=False
             if (regex!=None):
